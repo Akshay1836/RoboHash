a22b16f6100525f97949a932556512b3d0206766
Akshay V M <akshayvm36@gmail.com>
2023-12-19 11:29:18 +0530

    h1 correction

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ export default class Hero extends Component {
   render() {
     return (
       <div className='title'>
-        <h1>ROBO-HASH</h1>
+        <h1>ROBOHASH</h1>
       </div>
     )
   }
